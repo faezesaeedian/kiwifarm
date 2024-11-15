d8a5d4594fcf04c74dc26b7a5f7bd5db60ff1c14
faezesaeedian <faezehsaedian63@gmail.com>
2024-11-15 15:13:42 +0100

    command lines par

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from utils import Utils
+import argparse
+
 
 def main():
     try:
         # Initialize the Utils class (loads config)
         utils = Utils()
 
-        # Get the start position from the user
-        start_x, start_y = map(int, input("Enter the start position (x y) separated by space: ").split())
-        start = (start_x, start_y)
+        # # Get the start position from the user
+        # start_x, start_y = map(int, input("Enter the start position (x y) separated by space: ").split())
+        # start = (start_x, start_y)
 
-        # Get the end position from the user
-        end_x, end_y = map(int, input("Enter the end position (x y) separated by space: ").split())
-        end = (end_x, end_y)
+        # # Get the end position from the user
+        # end_x, end_y = map(int, input("Enter the end position (x y) separated by space: ").split())
+        # end = (end_x, end_y)
 
-        # Validate inputs
-        if not (utils.is_within_bounds(start_x, start_y) and utils.is_within_bounds(end_x, end_y)):
-            print("Error: Start or end position is out of bounds. Please enter values between 0 and 7.")
-            return
+        # # Validate inputs
+        # if not (utils.is_within_bounds(start_x, start_y) and utils.is_within_bounds(end_x, end_y)):
+        #     print("Error: Start or end position is out of bounds. Please enter values between 0 and 7.")
+        #     return
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("start", help="Start position (x y)", type=str)
+        parser.add_argument("end", help="End position (x y)", type=str)
+
+        args = parser.parse_args()
+
+        start = tuple(map(int, args.start.split()))
+        end = tuple(map(int, args.end.split()))
+
 
         # Find all shortest paths
         all_paths = utils.bfs_knight_paths(start, end)
